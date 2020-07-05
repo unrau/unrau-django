fd1d1521859e26ea39909bd5d8beaee30f448ccf
Dax <jamie@unrau.me>
2020-07-05 14:12:39 -0700

    render content of music posts from database

--- music/views.py
+++ music/views.py
@@ -4,9 +4,12 @@ from django.views import generic
 from .models import MusicEntry
 
 class MusicList(generic.ListView):
+    model = MusicEntry
+    context_object_name = 'music_list'
     queryset = MusicEntry.objects.filter(status=1).order_by('-pub_date')
-    template_name = 'index.html'
+    template_name = 'music.html'
 
 class MusicDetail(generic.DetailView):
     model = MusicEntry
+    #context_object_name = 'music_entry'
     template_name = 'music_detail.html'
