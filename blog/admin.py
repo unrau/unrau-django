91f13b3ce1e7bf5ceb2edaf8fdb2e7e735a5ac46
Dax <jamie@unrau.me>
2020-07-09 14:25:16 -0700

    expand the blog functionality to match music entries

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import BlogEntry
 
-admin.site.register(BlogEntry)
+class BlogAdmin(SummernoteModelAdmin):
+    list_display = ('title_text', 'slug', 'pub_date', 'status')
+    list_filter = ("status",)
+    search_fields = ['title_text', 'content_text']
+    prepopulated_fields = {'slug': ('title_text',)}
+    summernote_fields = ('content_text',)
+
+admin.site.register(BlogEntry, BlogAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+from .models import BlogEntry
 
-def index(request):
-    return HttpResponse("BLOG PAGE")
+class BlogList(generic.ListView):
+    model = BlogEntry
+    context_object_name = 'blog_list'
+    queryset = BlogEntry.objects.filter(status=1).order_by('-pub_date')
+    template_name = 'blog.html'
+    paginate_by = 6
+
+class BlogDetail(generic.DetailView):
+    model = BlogEntry
+    template_name = 'blog_detail.html'
