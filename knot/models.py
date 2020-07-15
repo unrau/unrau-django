0304ca33667815901bfc702c525dd596a41b4e98
Dax <jamie@unrau.me>
2020-07-15 10:00:12 -0700

    require KotW page number field to be unique

--- knot/models.py
+++ knot/models.py
@@ -11,7 +11,7 @@ STATUS = (
 class KnotEntry(models.Model):
     title_text = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
-    page_num = models.IntegerField(default=0)
+    page_num = models.IntegerField(default=0, unique=True)
     comic_image = models.ImageField(upload_to='kotw')
     content_text = models.TextField()
     pub_date = models.DateTimeField('date published')
