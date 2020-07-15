8edc724991a88ce13eeccef237867e136e8d624e
Dax <jamie@unrau.me>
2020-07-15 09:57:17 -0700

    add page number to Knot of this World comic entries

--- knot/models.py
+++ knot/models.py
@@ -11,6 +11,7 @@ STATUS = (
 class KnotEntry(models.Model):
     title_text = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
+    page_num = models.IntegerField(default=0)
     comic_image = models.ImageField(upload_to='kotw')
     content_text = models.TextField()
     pub_date = models.DateTimeField('date published')
