b913b97b45640bc8f0808aac715a79a0688ed35c
Dax <jamie@unrau.me>
2020-07-15 15:04:17 -0700

    add pagination links to Knot comic detail template

--- knot/models.py
+++ knot/models.py
@@ -21,3 +21,24 @@ class KnotEntry(models.Model):
 
     def __str__(self):
         return self.title_text
+
+    def _prev_page(self):
+        prevpage = self.page_num - 1
+        if prevpage >= 0:
+            return prevpage
+        else:
+            return 0
+    previous = property(_prev_page)
+
+    def _next_page(self):
+        latestpage = KnotEntry.objects.latest('page_num')
+        nextpage = self.page_num + 1
+        if nextpage < latestpage.page_num:
+            return nextpage
+        else:
+            return latestpage.page_num
+    next = property(_next_page)
+
+    def _latest_page(self):
+        return KnotEntry.objects.latest('page_num').page_num
+    latest = property(_latest_page)
